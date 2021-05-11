aed46c4d09e66e917e8db9648320e29f3fc40702
Abdurrahman Abu-Hijleh <aabuhijleh@asaltech.com>
2021-05-11 19:30:46 +0300

    chore: update deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,50 @@
-const { resolve } = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const TerserWebpackPlugin = require('terser-webpack-plugin');
+const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
 const config = {
-  mode: isProd ? 'production' : 'development',
+  mode: isProd ? "production" : "development",
   entry: {
-    index: './src/index.tsx',
+    index: "./src/index.tsx",
   },
   output: {
-    path: resolve(__dirname, 'dist'),
-    filename: '[name].js',
+    path: resolve(__dirname, "dist"),
+    filename: "[name].js",
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'babel-loader',
+        use: "babel-loader",
         exclude: /node_modules/,
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Babel + TypeScript + React = ❤️',
-      template: 'src/index.html',
+      title: "Babel + TypeScript + React = ❤️",
+      template: "src/index.html",
     }),
   ],
 };
 
 if (isProd) {
   config.optimization = {
-    minimizer: [
-      new TerserWebpackPlugin(),
-    ],
+    minimizer: [new TerserWebpackPlugin()],
   };
 } else {
   // for more information, see https://webpack.js.org/configuration/dev-server
   config.devServer = {
-    port: 8080,
+    port: 3003,
     open: true,
     hot: true,
     compress: true,
-    stats: 'errors-only',
+    stats: "errors-only",
     overlay: true,
   };
 }
