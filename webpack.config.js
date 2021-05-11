cedbda63aac62f716c5fe223cb87f4c13e4c00e5
Abdurrahman Abu-Hijleh <aabuhijleh@asaltech.com>
2021-05-11 19:52:14 +0300

    chore: add sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,18 @@ const config = {
         use: "babel-loader",
         exclude: /node_modules/,
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   plugins: [
